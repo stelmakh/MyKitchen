73c4287e7059f82c26fde072033d91355b7f6827
Volodymyr Stelmakh <vk.stelmakh@gmail.com>
2020-04-19 13:57:16 +0300

    Add initial migration

---
+++ MyKitchen/Migrations/20200419105544_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyKitchen.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Carbs = table.Column<float>(nullable: false),
+                    Fats = table.Column<float>(nullable: false),
+                    Protein = table.Column<float>(nullable: false),
+                    Calories = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
