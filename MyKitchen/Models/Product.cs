8fa52f9b31df3d5571fc5e0ec9a1d3182e27cd49
Volodymyr Stelmakh <vk.stelmakh@gmail.com>
2020-04-19 20:07:01 +0300

    Finalize localization

--- MyKitchen/Models/Product.cs
+++ MyKitchen/Models/Product.cs
@@ -8,21 +8,27 @@ namespace MyKitchen.Models
         public int Id { get; set; }
 
         [Required, StringLength(60)]
+        [Display(Name = "Name")]
         public string Name { get; set; }
 
         [Required, Range(1,100)]
+        [Display(Name = "Carbs")]
         public float Carbs { get; set; }
 
         [Required, Range(1, 100)]
+        [Display(Name = "Fats")]
         public float Fats { get; set; }
 
         [Required, Range(1, 100)]
+        [Display(Name = "Protein")]
         public float Protein { get; set; }
 
         [Required]
+        [Display(Name = "Calories")]
         public float Calories { get; set; }
 
         [StringLength(250)]
+        [Display(Name = "Notes")]
         public string Notes { get; set; }
     }
 }
